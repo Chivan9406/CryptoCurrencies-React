59f1d38f99ac38058d9dc35c375a15bb0f3063bf
Chivan9406 <ivan9406@hotmail.com>
2025-08-26 13:38:49 -0600

    first commit

---
+++ src/components/CryptoSearchForm.tsx
@@ -0,0 +1,91 @@
+import { type ChangeEvent, type FormEvent, useState } from 'react'
+
+import ErrorMessage from './ErrorMessage'
+import type { Pair } from '../types'
+import { currencies } from '../data'
+import { useCryptoStore } from '../store/store'
+
+function CryptoSearchForm() {
+  const cryptoCurrencies = useCryptoStore(state => state.cryptoCurrencies)
+  const fetchData = useCryptoStore(state => state.fetchData)
+  const [ pair, setPair ] = useState<Pair>({
+    currency: '',
+    cryptoCurrency: ''
+  })
+  const [ error, setError ] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setPair({
+      ...pair,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>)=> {
+    e.preventDefault()
+
+    if (Object.values(pair).includes('')) {
+      setError('Todos los campos son obligatorios')
+      return
+    }
+
+    setError('')
+    fetchData(pair)
+  }
+
+  return (
+    <form
+      className="form"
+      onSubmit={ handleSubmit }
+    >
+      { error && <ErrorMessage>{ error }</ErrorMessage>}
+
+      <div className="field">
+        <label htmlFor="currency">Moneda:</label>
+        <select
+          name="currency"
+          id="currency"
+          value={ pair.currency }
+          onChange={ handleChange }
+        >
+          <option value="">-- Seleccione --</option>
+          { currencies.map(currency => (
+            <option
+              key={ currency.code }
+              value={ currency.code }
+            >
+              { currency.name }
+            </option>
+          )) }
+        </select>
+      </div>
+
+      <div className="field">
+        <label htmlFor="cryptoCurrency">Criptomoneda:</label>
+        <select
+          name="cryptoCurrency"
+          id="cryptoCurrency"
+          value={ pair.cryptoCurrency }
+          onChange={ handleChange }
+        >
+          <option value="">-- Seleccione --</option>
+          { cryptoCurrencies.map(crypto => (
+            <option
+              key={ crypto.CoinInfo.Name }
+              value={ crypto.CoinInfo.Name }
+            >
+              { crypto.CoinInfo.FullName }
+            </option>
+          )) }
+        </select>
+      </div>
+
+      <input
+        type="submit"
+        value="Cotizar"
+      />
+    </form>
+  )
+}
+
+export default CryptoSearchForm
---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from 'react'
+
+function ErrorMessage({ children }: { children: ReactNode}) {
+  return (
+    <div>
+      { children }
+    </div>
+  )
+}
+
+export default ErrorMessage
